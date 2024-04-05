4495f3dcd70e1a06f078d65ad6a2fdd49d5a7d5c
Owain Williams <owain@andersdigital.com>
2024-04-05 21:20:20 +0100

    add new users to db

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,44 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 
-export default function TempPage() {
+export default async function TempPage() {
+  const { userId, orgRole } = auth();
+  if (!userId) {
+    return;
+  }
+  const dbUser = await db.user.findUnique({
+    where: {
+      clerkId: userId,
+    },
+  });
+  if (!dbUser) {
+    const newDbUser = await db.user.create({
+      data: {
+        clerkId: userId,
+      },
+    });
+  }
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
       </div>
       <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
         <div className="flex flex-col items-center gap-1 text-center">
           <h3 className="text-2xl font-bold tracking-tight">
-            You have no products
+            You have no dogs added
           </h3>
           <p className="text-sm text-muted-foreground">
-            You can start selling as soon as you add a product.
+            You can start making bookings as soon as you add your Dog.
           </p>
-          <Button className="mt-4">Add Product</Button>
+          <Button className="mt-4">Add Dog</Button>
         </div>
       </div>
     </main>
